a5a6528751e2c356e09e675dbe90166ce2bee5d3
wandakim <nwd1583@naver.com>
2021-05-02 17:09:19 +0900

    [JSconcept] - Eventloop, allstack

--- JavaScript/javascript-concepts/eventloope.js
+++ JavaScript/javascript-concepts/eventloope.js
@@ -1,5 +1,23 @@
 function yo() {
     setTimeout(() => {
-        console.lgo(1.5s Timer done!)
+        console.log(`1.5s Timer done!`)
     }, 1500);
+
+    for (let i = 0; i<3; i++){
+    doSomething();
+    console.log(i)
 }
+
+}
+function doSomething() { // do something for 3sec
+    console.log(`it takes 3sec`);
+};
+
+yo();
+
+console.log('main 끝!');
+
+/**
+ * 콜스택에 실행중인 일을 멈추고 다른 일을 할 수 없다. 
+ * 콜스택에서 다른 함수를 처리하고 있을 때 API에서 타임아웃 된 함수를 큐에 넣어주면 처리중인 것이 끝날 때까지 큐에서 대기한다. 
+ */
