c2d4605cc301a429bb6c86508e20b84540437c6e
wandakim <nwd1583@naver.com>
2021-05-04 16:51:47 +0900

    JSX, make component

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Potato from './Potato'; 
 
 function App() {
-  return <div>hello!!!!</div>
+  return (
+  <div>
+    <h1>hello!!!!</h1>
+    <Potato />
+  </div>  
+  );
 }
 
 export default App;
---
+++ React/movie_app/src/Potato.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Potato() {
+    return <h1>Photato Chipdddd</h1>
+}
+
+export default Potato;
