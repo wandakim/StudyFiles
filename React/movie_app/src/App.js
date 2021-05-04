7f89e82abdbd3037b37a1a59e0ac854309e0bd76
wandakim <nwd1583@naver.com>
2021-05-04 22:38:02 +0900

    JSX + props

--- React/movie_app/src/App.js
+++ React/movie_app/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Potato from './Potato'; 
+
+function Potato({ name }) {
+  return <h1>I like {name}</h1>;
+}
 
 function App() {
   return (
   <div>
     <h1>hello!!!!</h1>
-    <Potato />
+    <Potato name = "bigpo"/>
+    <Potato name = "smallpo"/>
+    <Potato name = "midpo"/>
+    <Potato name = "illpo"/>
+    <Potato name = "gimpo"/>
   </div>  
   );
 }
+
+// JSX is HTML +JavaScript. 
+// components start with upper case. 
+// we can pass an information to componants.
+// props.
 
 export default App;
--- React/movie_app/src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Potato() {
-    return <h1>Photato Chipdddd</h1>
-}
-
-export default Potato;
