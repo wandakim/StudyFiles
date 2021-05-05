975b11f29b2b198add3f59acc5badf2cc21e6a64
wandakim <nwd1583@naver.com>
2021-05-06 02:16:33 +0900

    boj1330

---
+++ Algorithm/1330.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().split(' ').map(val => +val);
+const [a,b] = input;
+
+
+switch(true){
+    case (a > b): console.log('>');
+        break;
+    case (a < b): console.log('<');
+        break;
+    case (a == b): console.log('==');
+        break;
+}
--- Algorithm/2255.js
+++ Algorithm/2255.js
@@ -7,3 +7,15 @@ console.log(A * parseInt(B[2]));
 console.log(A * parseInt(B[1]));
 console.log(A * parseInt(B[0]));
 console.log(A * parseInt(B));
+
+
+--더 간단하게 풀어봄
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().split('\n');
+const A = +input[0];
+const B = input[1];
+
+console.log(A * (+B[2]));
+console.log(A * (+B[1]));
+console.log(A * (+B[0]));
+console.log(A * (+B));
