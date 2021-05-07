52cd4eef3c8b656dd886d8baa903f99947d2549f
wandakim <nwd1583@naver.com>
2021-05-07 16:26:37 +0900

    setInterval,clearInterval

--- JavaScript/javascript-concepts/setInterval.js
+++ JavaScript/javascript-concepts/setInterval.js
@@ -1,4 +1,5 @@
 let i = undefined;
+let k = undefined;
 i = setInterval(()=> {
     console.log('hello');
 },1000); // 1초마다 'hello'를 출력한다. 
@@ -16,3 +17,4 @@ setTimeout(()=> {
 },7500);// 7.5초가 지나면 'hoizza'가 출력되는 것을 멈춘다. 
 
 // setInterval() 은 고유의 id를 출력하고 그 id를 clearInterval()에 전달해주어 멈출 수 있다. 
+// i와 k를 undefined로 할당해준 이유임 
