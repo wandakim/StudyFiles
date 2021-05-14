166493baccf3cbb023437f41f1e7853e273094c8
wandakim <nwd1583@naver.com>
2021-05-15 08:54:35 +0900

    git error

--- HtmlCss/Youtube_Clone/main.js
+++
@@ -1,7 +0,0 @@
-const moreBtn = document.querySelector('.info .metadata .moreBtn');
-const title = socument.querySelector('.info .metadata . title');
-
-moreBtn.addEventListener('click', () => {
-    moreBtn.classList.toggle('clicked');
-    title.classList.toggle('clamp');
-}); 
