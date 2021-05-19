b745f8b1ff1c42aa4ad8a67ea170685ffab6c125
wandakim <nwd1583@naver.com>
2021-05-19 13:32:29 +0900

    for 10.별찍기2

--- Algorithm/test.js
+++ Algorithm/test.js
@@ -1,7 +1,16 @@
 
 const N = 9;
 const star = [];
-for (i=0;i<N;i++){
-    star.push('*')
-    console.log(star.join(''));
+const blank = [];
+
+for(i = 0; i < N ; i++) {
+    blank.push(' ')
+}
+
+for (i = 0; i < N; i++) {
+    star.push('*');
+    let stars = star.join('');
+    blank.pop();
+    let blanks = blank.join('');
+    console.log(`${blanks}${stars}`)
 }
