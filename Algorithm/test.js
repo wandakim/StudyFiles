ad7bfe2d7e699b355bb3bfed092c39c5c9d5fa35
wandakim <nwd1583@naver.com>
2021-05-19 12:38:09 +0900

    11022

--- Algorithm/test.js
+++ Algorithm/test.js
@@ -1,9 +1,9 @@
 
 
 
-const input = [2, '2 4', '3 6'];
+const input = [2, '9 9', '3 6'];
 const N = input.splice(0,1)*1;
 for (i=1; i<N+1; i++){
-    const [a,b] = input[i-1].split(' ');
-    console.log(`Case #${i}: ${a*1+b*1}`)
+    const [a,b] = input[i-1].split(' '); 
+    console.log(`Case #${i}: ${a} + ${b} = ${a*1+b*1}`)
 }
