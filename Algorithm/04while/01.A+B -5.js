f2fca2be239d3d13523e1d34c02adc4487c91bac
wandakim <nwd1583@naver.com>
2021-05-29 19:30:56 +0900

    boj while 01

--- Algorithm/04while/01.A+B -5.js
+++ Algorithm/04while/01.A+B -5.js
@@ -34,8 +34,45 @@ const input = fs.readFileSync('dev/stdin').toString().split('\n');
 let i = 0;
 while(a+b !== 0)
 { 
-    let [a,b] = input.[i].split(' ').map(v=>+v);
+    let [a,b] = input[i].split(' ').map(v=>+v);
     console.log(a+b);
     i++;
 }
 // 제출1, 런타임 에러. test 진행
+
+// 문제 조건을 보니 b 는 음수일 수 있다. 
+// 논리 연산자를 사용해서 풀이 시도. 
+let i = 0;
+while(a !== 0 && b!== 0)
+{ 
+    let [a,b] = input[i].split(' ').map(v=>+v);
+    console.log(a+b);
+    i++;
+}
+
+// 왜 틀렸을까.. while 안에 논리연산자를 사용하면 안되나?  
+// 논리 연산자 사용에는 문제가 없었다. test를 돌려보니 밖에서 정해진 a,b를 가지고 while이 시작될 때 검사를 한다. 즉, undefined가 된다. 
+
+input.forEach((e)=>{
+    let [a,b] = e.split(' ').map(v=>+v);
+    while(a !== 0 && b!== 0) 
+    { 
+        console.log(a+b);
+        break;
+    }
+}    
+)
+// 맞았습니다!! 
+// 하나씩 가져와서 쪼개고 while을 돌렸다. 그리고 break 로 멈추어 줬다.
+// 근데 이렇게 하려면 차라리 if 하는거나 다름이 없다는 생각이 듬. 
+input.forEach((e)=>{
+    let [a,b] = e.split(' ').map(v=>+v);
+    if(a !== 0 && b!== 0) 
+    { 
+        console.log(a+b);
+    }
+}    
+) 
+// if 를 사용하면 break 할 필요도 없다. 
+// while로 풀어보라는 것 같은데 다른 언어를 보니 while을 이용하면 간편하게 풀 수 있는 방법이 있는 것 같다. 
+// 다른 언어는 입력을 바로바로 받아서 처리를 할 수 있어서 그런가 입력처리가 단순해보인다. 
--- Algorithm/test.js
+++ Algorithm/test.js
@@ -1,17 +1,12 @@
 'use strict'
 
-const   input = ['1 2','3 4 3 4 2 3 4'];
-const NL = input.splice(0,1).toString().split(' '); 
-const N = NL[0]*1;
-const L = NL[1]*1;
+const input = ['1 2', '3 4', '0 0']
 
-console.log(input);
-console.log(input[0]);
-console.log(input.toString());
-const numbersArray = input[0].toString().split(' ').map(val=>+val);
-
-console.log(numbersArray);
-
-// 근데 틀린 이유를 모르겠다. input.toString() 그리고 input[0] 둘다 같은 문자열을 콘솔에 출력하는데 왜 전자만 오류가 나는가? 
-// 애초에 배열 안에 원소 하나 들어 있는 것을 그냥 꺼내는 것이 나은 방법이기는 한데 무엇 때문에 틀리는지는 모르겠다. 
-// 일단 배열에 원소가 하나 남았을 경우는 그냥 꺼내서 쓰자.. 
+let i = 0;
+let [a,b] = input[i].split(' ').map(v=>+v);
+while(a !== 0 && b!== 0)
+{ 
+    console.log(a+b);
+    i++;
+    break;
+}
