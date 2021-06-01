7330f1aac72ceaef8e66d69466b43e85d2084da9
wandakim <nwd1583@naver.com>
2021-06-01 21:27:53 +0900

    boj while 02

--- Algorithm/test.js
+++ Algorithm/test.js
@@ -1,13 +1,11 @@
 'use strict'
 
-const input = ['1 2', '3 4', '0 0'];
-
-let n = input.length
-let i = 0;
-while (i < n){
-    let [a , b] = input[i].split(' ').map(v=>+v);
-    console.log(a + b);
-    i++;
+const input = ['1 2', '3 4', '0 0', '9 10'];
+const n = input.length
+for (let i = 0; i < n-1; i++){
+    let [a , b] = input[i].split(' ').map(v => +v);
+    console.log(a);
+    console.log(b);
 }
 //test.js:8 Uncaught TypeError: Cannot read property 'split' of undefined at test.js:8
 
@@ -20,3 +18,9 @@ while (i < n){
 // VM353:1 Uncaught SyntaxError: Identifier 'input' has already been declared
 //내가 뭔가 착각하고 있는것같다. 
 //뜯어보니.. input.i가 아니라 input[i]로 했어야 하는데...
+
+//3
+//7
+//0
+
+// 
